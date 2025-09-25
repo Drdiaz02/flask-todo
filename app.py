e6701da5b8ccbf3f55775c3100a050aaf686eb56
Jon Craton <jncraton@gmail.com>
2025-09-25 15:55:11 -0400

    Add app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import send_file, request, redirect
+from html import escape
+
+app = Flask(__name__)
+
+template = """
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <title>My Todo List</title>
+  </head>
+
+  <body>
+    <main>
+      <h1>My Todo List</h1>
+      <ul>
+      {items}
+      </ul>
+      <form action="/items" method="post">
+        <input type="text" name="item_add">
+        <input type="submit" value="Add Item">
+      </form>
+    </main>
+  </body>
+</html>
+""".strip()
+
+items = ["Finish Web Apps Project"]
+
+def get_item_component(item_text):
+    return f"""
+        <li>
+        <form action="/items" method="post">
+            <input type="submit" value="Mark Complete">
+            <input type="text" name="item_del" value="{item_text}" readonly>
+        </form>
+        </li>
+    """
+
+@app.route('/items', methods=["GET"])
+def list_items():
+    html = "".join([get_item_component(li) for li in items])
+
+    return template.replace("{items}", html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
